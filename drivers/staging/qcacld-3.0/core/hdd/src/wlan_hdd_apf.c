3d98472ba28ebaf136c0d69ebf10a0e470a80f13
Ashish Kumar Dhanotiya <adhanoti@codeaurora.org>
2019-07-26 12:12:56 +0530

    qcacld-3.0: Possible integer overflow in hdd apf read memory cb
    In hdd_apf_read_memory_cb, context buffer length is checked
    against sum of packet offset and event length, packet offset
    and event length are extracted from FW response and can lead
    to integer overflow, which will allow to pass the length check
    and eventually will lead to buffer overwrite when event data is
    copied to context buffer.
    To avoid this issue, validate the event length against the
    available length in the context buffer, which can be obtained
    by getting difference of packet offset from the context buffer
    length.
    
    Change-Id: I53798e56403f1c550f0a762645ccd67a1dc8500d
    CRs-fixed: 2436502

--- drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_apf.c
+++ drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_apf.c
@@ -471,6 +471,7 @@ hdd_apf_read_memory_callback(void *hdd_context,
 
 	if ((pkt_offset > context->buf_len) ||
 	    (context->buf_len - pkt_offset < read_mem_evt->length)) {
+		qdf_spin_unlock(&context->lock);
 		hdd_err("Read chunk exceeding allocated space");
 		return;
 	}
